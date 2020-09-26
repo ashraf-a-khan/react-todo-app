a76deb7df1d3ddc3d4fe7ccbb8e75537229d1f8f
ashraf-a-khan <khan.ashraf2311@gmail.com>
2020-09-26 14:15:21 -0400

    Updates

---
+++ precache-manifest.afc0ca6ec1f0234d7fb02f8b1c3ae362.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f727c914e244353a019308a85de4a50",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "1927942b795c1bd62630",
+    "url": "/react-todo-app/static/css/main.d12423a4.chunk.css"
+  },
+  {
+    "revision": "e4ea4ab78812acb1eaa9",
+    "url": "/react-todo-app/static/js/2.50453ecc.chunk.js"
+  },
+  {
+    "revision": "dd207babaeb913476be46155cc504ef5",
+    "url": "/react-todo-app/static/js/2.50453ecc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1927942b795c1bd62630",
+    "url": "/react-todo-app/static/js/main.d5ea7424.chunk.js"
+  },
+  {
+    "revision": "38bd6552324fcc0edf2e",
+    "url": "/react-todo-app/static/js/runtime-main.a467fb68.js"
+  }
+]);
